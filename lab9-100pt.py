844ba55b1f6111cacf1fb427847562d16b5a5bba
Hannah-Pierre <s581460@student.roundrockisd.org>
2014-10-15 15:40:50 -0500

    100pt lab (unfinished)

--- lab9-100pt.py
+++ lab9-100pt.py
@@ -13,3 +13,20 @@
 # - A temperature of over 105F
 # - A temperature of over 102F and they have been sick in the last 24 hours
 # - A temperature over 100, OR they've been sick in the last 24 hours, AND they've recently travelled to West Africa.
+Patientinfo = []
+print "what is your temperature?"
+usertemp = raw_input()
+print "Have you been sick in the last 24 hours? 1 for no 2 for yes."
+usersick = raw_input()
+print "have you recently travelled to West Africa? 1 for no 2 for yes"
+userafrica = raw_input()
+
+
+while() :
+    if usertemp in patientinfo > 105 :
+        print "you're sick" 
+    if usertemp in patientinfo > 102 and usersick == 2 :
+        print "you're sick"
+    if usertemp in patientinfo > 100 or usersick in patientinfo == 2 and userafrica in patientinfo == 2 :
+        print "you're sick"
+        
