1908b4106186eae68f19bd0e87b658178a58d9e7
Hannah-Pierre <s581460@student.roundrockisd.org>
2014-10-15 15:17:56 -0500

    70pt lab

--- lab9-70pt.py
+++ lab9-70pt.py
@@ -12,3 +12,9 @@
 # Accept user input 
 # Calculate fahrenheit
 # Output answer
+print "input a number to be changed from celcius to fahrenheit:"
+userinput = int(raw_input())
+userinput2 = userinput * 9
+userinput3 = userinput2 / 5
+userinput4 = userinput3 + 32
+print userinput4
